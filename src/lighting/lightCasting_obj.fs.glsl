ce796d86741a5829e88e097d768afde154bacee3
NWPU66 <18050023358@163.com>
2024-03-29 15:19:41 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   build/CMakeFiles/chapter_0.dir/objects.a
    	modified:   build/CMakeFiles/class_camera.dir/src/util/class_camera.cpp.obj
    	modified:   build/CMakeFiles/color.dir/objects.a
    	modified:   build/CMakeFiles/lighting.dir/objects.a
    	modified:   build/chapter_0.exe
    	modified:   build/color.exe
    	modified:   build/libclass_camera.a
    	modified:   build/lighting.exe
    	modified:   src/lighting/lightCasting.cpp
    	modified:   src/lighting/lightCasting_obj.fs.glsl
    	modified:   src/lighting/lightCasting_obj.vs.glsl

--- src/lighting/lightCasting_obj.fs.glsl
+++ src/lighting/lightCasting_obj.fs.glsl
@@ -12,6 +12,9 @@ struct Material
 
 struct Light
 {
+    //灯光的类型
+    int type;
+    
     vec3 position;
     float intensity;
     vec3 ambient;
@@ -34,38 +37,130 @@ out vec4 fragColor;
 //uniform
 uniform vec3 cameraPos;
 uniform Material material;
-uniform Light light;
+#define MAX_NUM_LIGHTS_SPUUORT 16
+uniform Light light[MAX_NUM_LIGHTS_SPUUORT];
+//function
+vec3 dirLighting(Light light,vec3 viewDir);
+vec3 pointLighting(Light light,vec3 normal,vec3 fragPos,vec3 viewDir);
+vec3 spotLighting(Light light,vec3 normal,vec3 fragPos,vec3 viewDir);
 
 void main(){
-    //diffusion
-    vec3 vecToLight=light.position-position;
-    // 聚光灯裁切
-    float spotLightCutOff=dot(-light.lightDir,normalize(vecToLight));
-    if(spotLightCutOff>light.innerCutOff){
-        spotLightCutOff=1.f;
-    }else{
-        spotLightCutOff=0.f;
+    vec3 norm=normalize(normal);
+    vec3 viewDir=normalize(cameraPos-position);
+    vec3 outputColor;
+    for(int i=0;i<MAX_NUM_LIGHTS_SPUUORT;i++){
+        if(light[i].type!=-1){
+            switch(light[i].type){
+                case 0:outputColor+=dirLighting(light[i],viewDir);break;
+                case 1:outputColor+=pointLighting(light[i],norm,position,viewDir);break;
+                case 2:outputColor+=spotLighting(light[i],norm,position,viewDir);break;
+            }
+        }
     }
-    float lightDistance=distance(vecToLight,vec3(0.f));
-    float diffusionFac=max(dot(normalize(vecToLight),normalize(normal)),0.f);
-    vec3 lightDistVec=vec3(1.f,lightDistance,pow(lightDistance,2));
-    float lightDropOff=1.f/dot(lightDistVec,light.dropOffFac);
-    //combine
+    fragColor=vec4(outputColor,1.f);
+    // fragColor=vec4(vec3(spotLightCutOff),1.f);
+}
+
+vec3 dirLighting(Light light,vec3 normal,vec3 viewDir){
+    vec3 lightDir=normalize(-light.lightDir);
+    
+    //diffusion
+    float diffusionFac=max(dot(lightDir,normal),0.f);
     vec3 diffColor=light.diffuse*texture(material.diffuseMap,uv).xyz;
-    vec3 diffusion=light.intensity*diffColor*diffusionFac*lightDropOff;
+    vec3 diffuse=light.intensity*diffColor*diffusionFac;
+    
+    //specular
+    vec3 halfVec=normalize(lightDir+viewDir);
+    float specularFac=pow(max(dot(halfVec,normal),0.f),material.shininess);
+    vec3 specularColor=texture(material.specularMap,uv).xyz*light.specular;
+    vec3 specular=light.intensity*specularFac*specularColor;
     
     //ambient
     vec3 ambient=light.ambient*material.ambient*texture(material.diffuseMap,uv).xyz;
     
-    //specular
-    vec3 viewDir=normalize(cameraPos-position);
-    vec3 halfVec=normalize(normalize(vecToLight)+viewDir);
-    float specularFac=pow(max(dot(halfVec,normalize(normal)),0.f),material.shininess);
     //combine
-    vec3 specularColor=texture(material.specularMap,uv).xyz*light.specular*material.specular;
+    return(diffuse+specular+ambient);
+}
+
+vec3 pointLighting(Light light,vec3 normal,vec3 fragPos,vec3 viewDir){
+    vec3 lightDir=normalize(light.position-fragPos);
+    
+    //diffuse
+    float lightDistance=distance(lightDir,vec3(0.f));
+    float diffusionFac=max(dot(lightDir,normal),0.f);
+    vec3 lightDistVec=vec3(1.f,lightDistance,pow(lightDistance,2));
+    float lightDropOff=1.f/dot(lightDistVec,light.dropOffFac);
+    vec3 diffColor=light.diffuse*texture(material.diffuseMap,uv).xyz;
+    vec3 diffuse=light.intensity*diffColor*diffusionFac*lightDropOff;
+    
+    //specular
+    vec3 halfVec=normalize(lightDir+viewDir);
+    float specularFac=pow(max(dot(halfVec,normal),0.f),material.shininess);
+    vec3 specularColor=texture(material.specularMap,uv).xyz*light.specular;
     vec3 specular=light.intensity*specularFac*specularColor*lightDropOff;
     
-    //final color
-    fragColor=vec4((ambient+(specular+diffusion)*spotLightCutOff),1.f);
-    // fragColor=vec4(vec3(spotLightCutOff),1.f);
+    //ambient
+    vec3 ambient=light.ambient*material.ambient*texture(material.diffuseMap,uv).xyz;
+    
+    //combine
+    return(diffuse+specular+ambient);
 }
+
+vec3 spotLighting(Light light,vec3 normal,vec3 fragPos,vec3 viewDir){
+    vec3 lightDir=normalize(light.position-fragPos);
+    // 聚光灯裁切
+    float spotLightCutOff=dot(-light.lightDir,lightDir);
+    float cutOffRange=light.innerCutOff-light.outerCutOff;
+    spotLightCutOff=clamp((spotLightCutOff-light.outerCutOff)/cutOffRange,0.f,1.f);
+    
+    //diffuse
+    float lightDistance=distance(lightDir,vec3(0.f));
+    float diffusionFac=max(dot(lightDir,normal),0.f);
+    vec3 lightDistVec=vec3(1.f,lightDistance,pow(lightDistance,2));
+    float lightDropOff=1.f/dot(lightDistVec,light.dropOffFac);
+    vec3 diffColor=light.diffuse*texture(material.diffuseMap,uv).xyz;
+    vec3 diffuse=light.intensity*diffColor*diffusionFac*lightDropOff*spotLightCutOff;
+    
+    //specular
+    vec3 halfVec=normalize(lightDir+viewDir);
+    float specularFac=pow(max(dot(halfVec,normal),0.f),material.shininess);
+    vec3 specularColor=texture(material.specularMap,uv).xyz*light.specular;
+    vec3 specular=light.intensity*specularFac*specularColor*lightDropOff*spotLightCutOff;
+    
+    //ambient
+    vec3 ambient=light.ambient*material.ambient*texture(material.diffuseMap,uv).xyz;
+    
+    //combine
+    return(diffuse+specular+ambient);
+}
+
+/* old one
+//diffusion
+vec3 vecToLight=light.position-position;
+// 聚光灯裁切
+float spotLightCutOff=dot(-light.lightDir,normalize(vecToLight));
+float cutOffRange=light.innerCutOff-light.outerCutOff;
+spotLightCutOff=clamp((spotLightCutOff-light.outerCutOff)/cutOffRange,0.f,1.f);
+//
+float lightDistance=distance(vecToLight,vec3(0.f));
+float diffusionFac=max(dot(normalize(vecToLight),normalize(normal)),0.f);
+vec3 lightDistVec=vec3(1.f,lightDistance,pow(lightDistance,2));
+float lightDropOff=1.f/dot(lightDistVec,light.dropOffFac);
+//combine
+vec3 diffColor=light.diffuse*texture(material.diffuseMap,uv).xyz;
+vec3 diffusion=light.intensity*diffColor*diffusionFac*lightDropOff;
+
+//ambient
+vec3 ambient=light.ambient*material.ambient*texture(material.diffuseMap,uv).xyz;
+
+//specular
+vec3 viewDir=normalize(cameraPos-position);
+vec3 halfVec=normalize(normalize(vecToLight)+viewDir);
+float specularFac=pow(max(dot(halfVec,normalize(normal)),0.f),material.shininess);
+//combine
+vec3 specularColor=texture(material.specularMap,uv).xyz*light.specular*material.specular;
+vec3 specular=light.intensity*specularFac*specularColor*lightDropOff;
+
+//final color
+fragColor=vec4((ambient+(specular+diffusion)*spotLightCutOff),1.f);
+*/
